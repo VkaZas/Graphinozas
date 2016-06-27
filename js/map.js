105e5dda8ba6d7bcddb0459878c9a60d190f50a2
vkazas <vkazas@163.com>
2016-06-27 16:58:59 +0800

    collsion test

--- js/map.js
+++ js/map.js
@@ -2,46 +2,7 @@
  * Created by Administrator on 2016/6/21.
  */
 
-function getAxisArr(len) {
-    len = len || 100;
-    var axis = [];
-    var g1 = new THREE.Geometry();
-    var g2 = new THREE.Geometry();
-    var g3 = new THREE.Geometry();
-    var p0 = new THREE.Vector3(0, 0, 0);
-    var p1 = new THREE.Vector3(len, 0, 0);
-    var p2 = new THREE.Vector3(0, len, 0);
-    var p3 = new THREE.Vector3(0, 0, len);
-
-    g1.vertices.push(p0, p1);
-    axis.push(new THREE.Line(
-        g1,
-        new THREE.LineBasicMaterial({color: 0xFF0000, opacity: 0.5})
-    ));
-    g2.vertices.push(p0, p2);
-    axis.push(new THREE.Line(
-        g2,
-        new THREE.LineBasicMaterial({color: 0x00FF00})
-    ));
-    g3.vertices.push(p0, p3);
-    axis.push(new THREE.Line(
-        g3,
-        new THREE.LineBasicMaterial({color: 0x0000FF})
-    ));
-    // axis.push(new THREE.ArrowHelper(
-    //     THREE.Vector3(0, 1, 0),
-    //     THREE.Vector3(0, 0, 0),
-    //     10, 0xFF0000, 3, 3));
-    // axis.push(new THREE.ArrowHelper(
-    //     THREE.Vector3(0, 0, 1),
-    //     THREE.Vector3(0, 0, 0),
-    //     10, 0x0000FF, 3, 3));
-    // axis.push(new THREE.ArrowHelper(
-    //     THREE.Vector3(1, 0, 0),
-    //     THREE.Vector3(0, 0, 0),
-    //     10, 0x00FF00, 3, 3));
-    return axis;
-}
+var pi = 3.1415926;
 
 function getGeoMap() {
     var geo = new THREE.PlaneGeometry(20, 20, 5, 5);
@@ -50,57 +11,82 @@ function getGeoMap() {
         geo,
         new THREE.MeshLambertMaterial({map:texture})
     );
+    geoObj.rotation.set(-pi*3/2,0,0);
     return geoObj;
 }
 
-function getRobot() {
-    var manager = new THREE.LoadingManager();
-    var obj;
-    manager.onProgress = function(item, loaded, total) {
-        console.log(item, loaded, total);
-    };
-    var loader = new THREE.ObjectLoader(manager);
-    loader.load('./models/robot.obj',function(object) {
-        object.traverse(function(child) {
-            
-        });
-        object.updateMatrix();
-        obj = object;
-    });
-    return obj;
+function getMapArr(size, ofsX, ofsZ) {
+    this.ofsX = (ofsX || 9 / 2) * size;
+    this.ofsZ = (ofsZ || 9 / 2) * size;
+    this.size = size || 10;
+    var cube = [];
+    var width	= 9;
+    var height 	= 9;
+    var maps = [0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 0, 1, 0, 1, 0, 0,
+        0, 1, 0, 0, 0, 0, 1, 1, 0,
+        0, 1, 0, 1, 0, 1, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 0, 1, 0, 1, 0,
+        0, 1, 1, 0, 0, 0, 0, 1, 0,
+        0, 0, 1, 0, 1, 0, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for(var i = 0; i < width * height; i++)
+        if(maps[i] == 1)
+        {
+            var tmpCube = new THREE.Mesh(
+                new THREE.CubeGeometry(this.size,this.size,this.size),
+                new THREE.MeshLambertMaterial({color:0x0000FF})
+            );
+            tmpCube.position.set(
+                (i / width) * this.size - this.ofsX,
+                size / 2,
+                (i % width) * this.size - this.ofsZ);
+            cube.push(tmpCube);
+        }
+    return cube;
 }
 
-function getPlayer() {
-    var res = THREE.Object3D();
-    var onProgress = function ( xhr ) {
-        if ( xhr.lengthComputable ) {
-            var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log( Math.round(percentComplete, 2) + '% downloaded' );
-        }
-    };
+/*
+*  If the orgObj do not have vertices attribute, the function will build
+*  a border box for the orgObj using its lenX, lenY and lenZ attributes.
+* */
+function checkCollision(orgObj, collList) {
+    if (!collList || collList.length == 0) return false;
+    var verNum = (!!orgObj.vertices && orgObj.vertices.length) || 8;
+    var hx = orgObj.objSize.x / 2, hy = orgObj.objSize.y / 2, hz = orgObj.objSize.z / 2;
+    var ofsArr = [
+        new THREE.Vector3(hx, hy, hz),
+        new THREE.Vector3(hx, hy, -hz),
+        new THREE.Vector3(hx, -hy, hz),
+        new THREE.Vector3(hx, -hy, -hz),
+        new THREE.Vector3(-hx, hy, hz),
+        new THREE.Vector3(-hx, hy, -hz),
+        new THREE.Vector3(-hx, -hy, hz),
+        new THREE.Vector3(-hx, -hy, -hz)
+    ];
+    var verArr = [];
+    var originPoint = orgObj.position.clone();
 
-    var onError = function ( xhr ) { };
+    /*todo: make this generic*/
+    originPoint.y = 4;
 
-    THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+    // constructing the border box
+    for (var i=0; i<8; i++) {
+        var tmpVec = originPoint.clone().add(ofsArr[i]);
+        verArr.push(tmpVec);
+    }
+    // verArr = (!!orgObj.geometry && orgObj.geometry.vertices) || verArr;
 
-    var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setPath( './examples/obj/male02/' );
-    mtlLoader.load( 'male02_dds.mtl', function( materials ) {
+    for (var vertexIndex = 0; vertexIndex < verNum; vertexIndex++)
+    {
+        var localVertex = verArr[vertexIndex].clone();
+        // var globalVertex = localVertex.applyMatrix4( orgObj.matrix );
+        var directionVector = localVertex.sub( originPoint );
 
-        materials.preload();
-
-        var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials( materials );
-        objLoader.setPath( './examples/obj/male02/' );
-        objLoader.load( 'male02.obj', function ( object ) {
-
-            object.position.y = 0;
-            object.scale.x = object.scale.y = object.scale.z = 0.05;
-            object.rotation.set(1.585,0,0);
-            res = object;
-            return object;
-
-        }, onProgress, onError );
-
-    });
+        var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+        var collisionResults = ray.intersectObjects( collList );
+        if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
+            return true;
+    }
 }
